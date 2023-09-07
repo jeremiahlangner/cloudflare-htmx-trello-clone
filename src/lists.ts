e9b6071963d301916551bdae1d01a8016295048c
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-09-07 15:48:56 -0700

    housekeeping: rename main

--- src/lists.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "./router";
-import { AddList } from "./views/add_list";
-
-const router = new Router();
-router.register("/add", (params) => {
-  return AddList;
-});
-
-export default router.routes;
