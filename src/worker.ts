7c221e14145aee76e7c714a38a5e416da60ba352
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-09-08 13:36:12 -0700

    substitute git lib for router

--- src/worker.ts
+++ src/worker.ts
@@ -1,8 +1,8 @@
-import { Router } from "./router";
+import { Router } from 'simple-worker-router';
 import Index from "./views/index";
 import Board from "./views/board";
-import AddList from "./views/add_list";
-import NewList from "./views/new_list";
+// import AddList from "./views/add_list";
+// import NewList from "./views/new_list";
 
 const lists = [
   {
@@ -39,19 +39,23 @@ const lists = [
   },
 ];
 
-const router = new Router([
-  ["/", Index],
-  ["/", Board, "POST"],
-  /*  ["/add", AddList],
-  ["cancel", NewList],
-  ["/add/:id", AddCard],
-  ["/edit/:list_id/:id", EditCard],
-  ["/:list_id/:id", Card, "PUT"],
-  ["/cancel/:id", ToggleAddCard],
-  ["/cancel-edit/:list_id/:id", Card ],
-  ["/:list_id/:id", List, "DELETE"],
-  ["/move", Board, "POST"],
-  ["/new/:list_id", NewCard, "POST"], */
-]);
+export default {
+  async fetch(request: Request, env: Environment, ctx: any) {
+    const router = new Router([
+      ["/", Index],
+      ["/", Board, "POST"],
+      /*  ["/add", AddList],
+      ["cancel", NewList],
+      ["/add/:id", AddCard],
+      ["/edit/:list_id/:id", EditCard],
+      ["/:list_id/:id", Card, "PUT"],
+      ["/cancel/:id", ToggleAddCard],
+      ["/cancel-edit/:list_id/:id", Card ],
+      ["/:list_id/:id", List, "DELETE"],
+      ["/move", Board, "POST"],
+      ["/new/:list_id", NewCard, "POST"], */
+    ]);
+    return router.handle({ request, env, ctx });
+  },
+};
 
-export default router.handle;
