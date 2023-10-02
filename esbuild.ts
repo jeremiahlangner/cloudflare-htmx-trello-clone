c86a813f92762cb14ddc8ac3417ee18c6c20cce6
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-10-01 20:18:45 -0700

    add annotations to build

--- esbuild.ts
+++ esbuild.ts
@@ -2,13 +2,15 @@ import { build } from "esbuild";
 import { readFileSync, writeFileSync } from "fs";
 async function Build() {
   try {
+    // build styles first to minify and implement into dist worker
     await build({
-      entryPoints: ["./assets/styles.css"],
+      entryPoints: ["./assets/styles.css"], // TODO: convert to SCSS
       outfile: "./assets/styles.min.css",
       minify: true,
       bundle: true,
     });
 
+    // build service worker file to integrate in bundle as servable text file
     await build({
       entryPoints: ["./src/sw.ts"],
       outfile: "./assets/sw.js",
@@ -18,16 +20,19 @@ async function Build() {
       treeShaking: true,
     });
 
+    // add styles to build
     const styles = readFileSync("assets/styles.min.css", { encoding: "utf8" });
     writeFileSync("src/templates/styles.json", JSON.stringify({ styles }), {
       encoding: "utf8",
     });
 
+    // add service worker to build
     const sw = readFileSync("assets/sw.js", { encoding: "utf8" });
     writeFileSync("src/templates/sw.json", JSON.stringify({ sw }), {
       encoding: "utf8",
     });
 
+    // build with all imports
     await build({
       entryPoints: ["./src/worker.ts"],
       outfile: "./build/worker.js",
@@ -37,7 +42,7 @@ async function Build() {
       treeShaking: true,
     });
   } catch (e) {
-    console.error(e);
+    console.error(e); // I prefer this to debug for something this simple.
     process.exit(1);
   }
 }
