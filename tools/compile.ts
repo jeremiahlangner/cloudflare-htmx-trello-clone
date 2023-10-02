23712debfc6cc0139392b63aeacd9085fb5fa455
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-10-01 20:21:21 -0700

    remove vestigial build folder

--- tools/compile.ts
+++
@@ -1,6 +0,0 @@
-import { readFileSync, writeFileSync } from "fs";
-
-const styles = readFileSync("assets/styles.min.css", { encoding: "utf8" });
-writeFileSync("src/templates/styles.json", JSON.stringify({ styles }), {
-  encoding: "utf8",
-});
